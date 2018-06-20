305491fb981b3ac819437860ed6d7a84eb0e0455
Essence Alexander <upperlefty@zoho.eu>
2018-06-20 11:36:37 -0700

    Create permute.js

---
+++ permute.js
@@ -0,0 +1,8 @@
+var checkPermute = function(stringOne, stringTwo) {
+  // if different lengths, return false
+  if (stringOne.length !== stringTwo.length) {
+    return false;
+  // else sort and compare 
+  // (doesn't matter how it's sorted, as long as it's sorted the same way)
+  } else {
+ 
